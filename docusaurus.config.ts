12a5b7044bdaf155287a7a3035fb5e188f306077
Adrian T <adu@tainic.ro>
2024-12-30 18:58:29 +0200

    fix: Update edit URL to point to the main branch for the repository.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/freebun-ai/website',
+            'https://github.com/freebun-ai/website/edit/main/',
         },
         blog: {
           showReadingTime: true,
@@ -53,7 +53,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/freebun-ai/website/',
+            'https://github.com/freebun-ai/website/edit/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
